8fd8f83e73b8fea1acd5ddf652b4c7fbb9a9816b
tiroa <95303626+tiroa@users.noreply.github.com>
2021-12-06 13:29:40 -0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,14 @@
+/**
+ * Prueba para formato de bloques
+ */
+//% color=190 weight=100 icon="\uf1ec" block="Mostrar Numero"
+namespace show {
+    //% blockId=device_show_number
+    //% block="show|number %v"
+    export function showNumber(v: number, interval: number = 150): void
+    { 
+        let a : string = v + '0';
+        basic.showString(a);
+    }
+    
+}
