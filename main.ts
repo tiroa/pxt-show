d083c66e3b676001b299ea0944cdcdbccd2763e2
tiroa <95303626+tiroa@users.noreply.github.com>
2021-12-06 13:45:34 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-/**
- * Prueba para formato de bloques
- */
+
 //% color=190 weight=100 icon="\uf1ec" block="Mostrar Numero"
 namespace show {
+    /**
+    * Prueba para formato de bloques
+    */
     //% blockId=device_show_number
     //% block="show|number %v"
     export function showNumber(v: number, interval: number = 150): void
